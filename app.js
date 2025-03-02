0b9b9ac2714f05f7093e4ed2cc053ea5170bb9e9
Imma-20 <152082464+Imma-20@users.noreply.github.com>
2025-03-02 02:00:19 +0100

    [ Etudiant 3 ] Mises à jour du fichier "app.j" avec le middleware d'authentification

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import { usersRoutes } from './src/routes/usersRoutes.js'
 import { spotlightsRoutes } from './src/routes/spotlightsRoutes.js'
 import { reservationsRoutes } from './src/routes/reservationsRoutes.js'
+import { authMiddleware } from './src/middlewares/authMiddleware.js'
 
 const app = express()
 const port = process.env.PORT || 3000;
@@ -14,19 +15,19 @@ app.get('/', (req, res) => res.send("WELCOME"))
 // Utilisateurs
 app.post('/register', usersRoutes.newUser)
 app.post('/login', usersRoutes.loginUser)
-app.get('/users', usersRoutes.showAllUsers)
-app.get('/users/:id_utilisateur', usersRoutes.showUser)
+app.get('/users', authMiddleware, usersRoutes.showAllUsers)
+app.get('/users/:id_utilisateur', authMiddleware, usersRoutes.showUser)
 app.put('/users', usersRoutes.updUser)
-app.delete('/users', authMiddleware,usersRoutes.delAllUsers)
-app.delete('/users/:id_utilisateur', authMiddleware,usersRoutes.delUser)
+app.delete('/users', authMiddleware, usersRoutes.delAllUsers)
+app.delete('/users/:id_utilisateur', authMiddleware, usersRoutes.delUser)
 app.get('/profile', authMiddleware, usersRoutes.getProfile)
 
 // Projecteurs
-app.post('/projectors', spotlightsRoutes.newSpotlight)
-app.get('/projectors', spotlightsRoutes.showAllSpotlights)
-app.get('/projectors/:id_projecteur', spotlightsRoutes.showSpotlight)
+app.post('/projectors', authMiddleware,spotlightsRoutes.newSpotlight)
+app.get('/projectors', authMiddleware, spotlightsRoutes.showAllSpotlights)
+app.get('/projectors/:id_projecteur', authMiddleware,spotlightsRoutes.showSpotlight)
 app.put('/projectors', spotlightsRoutes.updSpotlight)
-app.delete('/projectors', authMiddleware,spotlightsRoutes.delAllSpotlights)
+app.delete('/projectors', authMiddleware, spotlightsRoutes.delAllSpotlights)
 app.delete('/projectors/:id_projecteur', authMiddleware, spotlightsRoutes.delSpotlight)
 
 // Reservations
